a1f36c47b3bc542a973909a64fae9478b394f2a2
Enoch <enoch.liangtuang@gmail.com>
2025-02-26 00:09:16 +0530

    small_device_responsive

--- src/navigation/header/header.jsx
+++ src/navigation/header/header.jsx
@@ -7,12 +7,12 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
-// import { Link } from "react-router-dom";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import logo from "../../assets/images/images.png";
 import TopHeader from "../../component/top_header/top_header";
 import { FaEllipsisH } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
+import top_logo from "../../assets/images/top_logo.jpg";
 
 const navigation = [
   { name: "Maha Kumbh Mela 2025", href: "#", current: true },
@@ -34,78 +34,98 @@ function classNames(...classes) {
 export default function Header() {
   return (
     <div className="py-3">
-      <TopHeader />
+      <div className="hidden lg:block">
+        <TopHeader />
+      </div>
+      <div className="block lg:hidden">
+        <div className="flex justify-between items-center">
+          <div>
+            <img
+              src={top_logo}
+              alt="flag"
+              className="w-full h-[27px] object-cover"
+            />
+          </div>
+          <div className="flex gap-2">
+            <div className="text-xs font-bold">TODAY'S E PAPER</div>
+            <div>
+              <CiSearch />
+            </div>
+          </div>
+        </div>
+      </div>
       <Disclosure as="nav" className="bg-slate-100">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon
-                  aria-hidden="true"
-                  className="block size-6 group-data-[open]:hidden"
-                />
-                <XMarkIcon
-                  aria-hidden="true"
-                  className="hidden size-6 group-data-[open]:block"
-                />
-              </DisclosureButton>
-            </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex shrink-0 items-center">
-                <img
-                  alt="Your Company"
-                  src={logo}
-                  className="h-8 w-auto xs:none sm:none md:block"
-                />
+          <div className="relative flex h-16 items-center justify-between sm:hidden">
+            <div className="flex w-full justify-between items-center">
+              {/* Logo */}
+              <div className="flex items-center">
+                <img alt="Your Company" src={logo} className="h-8 w-auto" />
               </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  {visibleItems.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      aria-current={item.current ? "page" : undefined}
-                      className={classNames(
-                        item.current ? "text-black" : "hover:text-gray-500",
-                        "rounded-md px-3 py-2 text-sm font-medium"
-                      )}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
+              {/* Menu Button */}
+              <div className="flex items-center">
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon
+                    aria-hidden="true"
+                    className="block size-6 group-data-[open]:hidden"
+                  />
+                  <XMarkIcon
+                    aria-hidden="true"
+                    className="hidden size-6 group-data-[open]:block"
+                  />
+                </DisclosureButton>
+              </div>
+            </div>
+          </div>
+          <div className="hidden sm:flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex shrink-0 items-center">
+              <img
+                alt="Your Company"
+                src={logo}
+                className="h-8 w-auto xs:none sm:none md:block"
+              />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {visibleItems.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current ? "text-black" : "hover:text-gray-500",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
 
-                  {hiddenItems.length > 0 && (
-                    <Menu as="div" className="relative">
-                      <MenuButton className="flex items-center justify-center rounded-md px-3 py-2 text-gray-700 hover:text-gray-500">
-                        <FaEllipsisH className="h-6 w-6" />
-                      </MenuButton>
-                      <MenuItems className="absolute left-0 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                        {hiddenItems.map((item) => (
-                          <MenuItem key={item.name}>
-                            {({ active }) => (
-                              <a
-                                to={item.href}
-                                className={`block px-4 py-2 text-sm ${
-                                  active ? "bg-gray-100" : "text-gray-700"
-                                }`}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </MenuItem>
-                        ))}
-                      </MenuItems>
-                    </Menu>
-                  )}
-                </div>
-              </div>
-            </div>
-            <div className="absolute inset-y-0 right-0 flex items-center gap-2 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <div className="text-xs font-bold">TODAY'S E PAPER</div>
-              <div>
-                <CiSearch />
+                {hiddenItems.length > 0 && (
+                  <Menu as="div" className="relative">
+                    <MenuButton className="flex items-center justify-center rounded-md px-3 py-2 text-gray-700 hover:text-gray-500">
+                      <FaEllipsisH className="h-6 w-6" />
+                    </MenuButton>
+                    <MenuItems className="absolute left-0 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                      {hiddenItems.map((item) => (
+                        <MenuItem key={item.name}>
+                          {({ active }) => (
+                            <a
+                              to={item.href}
+                              className={`block px-4 py-2 text-sm ${
+                                active ? "bg-gray-100" : "text-gray-700"
+                              }`}
+                            >
+                              {item.name}
+                            </a>
+                          )}
+                        </MenuItem>
+                      ))}
+                    </MenuItems>
+                  </Menu>
+                )}
               </div>
             </div>
           </div>
